bef3033919cc815c4a3058955fa01064b29ba0d9
tkeda <tkeda@JAIGANESH>
2023-09-10 20:11:41 +0100

    Add project files.

---
+++ hello1/MathsOps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello1
+{
+    public class MathsOps
+    {
+        int a;
+        int b;
+        public static int add(int a, int b)
+        { 
+            return a + b;        
+        }
+
+        public int multiply(int _a, int _b)
+        {
+            this.a = _a;
+            this.b = _b;
+            return a * b;  
+        }
+    }
+}
---
+++ hello1/Program.cs
@@ -0,0 +1,10 @@
+﻿using hello1;
+using System;
+
+Console.WriteLine("hello world");
+
+
+Console.WriteLine(MathsOps.add(100,99)+"=is sum of 100 and 99");
+MathsOps math = new MathsOps();
+Console.WriteLine(math.multiply(5,4) + "= is multiplication of 5 and 4");
+Console.ReadKey();
